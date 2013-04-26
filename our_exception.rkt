a4d023f7a1b39eb428e92276f3c77c59d07c5414
Lorenzo Affetti <affox90@gmail.com>
2013-04-26 15:44:01 +0200

    now we have a handler object, and we catch right objects... I think...

--- our_exception.rkt
+++ our_exception.rkt
@@ -1,6 +1,21 @@
 #lang racket
+;we require the standard library for classes and objects
 (require racket/class)
 
+;the handler class
+;every handler contains:
+;- the class of the exception to be handled
+;- the lambda of the real oprations to be done (the real handler)
+(define handler%
+  (class object%
+    (super-new)
+    (init class handler)
+    (field (self-class class))
+    (field (self-handler handler))
+    (define/public (get-class) self-class)
+    (define/public (get-handler) self-handler)))
+
+;code for the stack which contains handlers
 (define *handlers* '())
 
 (define (push-h handler)
@@ -11,38 +26,76 @@
     (set! *handlers* (cdr *handlers*))
     handler))
 
-(define exception%
-  (class object%
-    (init message)
-    (define current-message message)
-    (super-new)
-    (define/public (get-message) current-message)))
+;only reads the first handler without popping it
+(define (read-h)
+  (car *handlers*))
 
-(define null-pointer-exception%
-  (class exception%
-    (super-new (message "null pointer"))))
+;throw method
+(define (can-handle? exception)
+  (if (empty? *handlers*)
+      #f ;no handlers... we can't handle
+      (let ((h (read-h))) ;there is at least one handler
+        (if (is-a? exception (send h get-class))
+            ;is the catch-class a superclass (or the same class as)
+            ;of the thrown-exception class?
+            #t
+            #f))))
 
 (define (throw exception)
-  (if (empty? *handlers*)
-      (error "EXCEPTION" (send exception get-message))
-      ((pop-h))))
+  (if (can-handle? exception)
+      ((send (pop-h) get-handler) exception)
+      (error "EXCEPTION:" (send exception get-message))))
 
 (define-syntax try
   (syntax-rules (catch)
-    ((try (exp1 ...) catch (exp2 ...))
-     (begin
+    ((try (exp1 ...) catch ((ex-class ex-name)) (exp2 ...))
      (call/cc (lambda (cont)
-                (push-h (lambda () (cont 
-                                    (begin exp2 ...))))
+                ;create a new handler
+                (let ((h (new handler%
+                             (class ex-class) (handler 
+                                               (lambda (ex-name) (cont 
+                                                           (begin exp2 ...)))))))
+                ;push the handler
+                (push-h h)
                 (let ((result (begin exp1 ...)))
                   (pop-h)
                   result)))))))
 
-(define (foo)
-  (throw (new exception% (message "Generic exception thrown"))))
+
+
+
+
+
+
+;some examples of classes
+(define exception%
+  (class object%
+    (super-new)
+    (init message)
+    (field (self-message message))
+    (define/public (get-message) self-message)))
+
+(define pippo-exception%
+  (class exception%
+    (super-new (message "pippo exception"))))
+
+(define pluto-exception%
+  (class exception%
+    (super-new (message "pluto exception"))))
 
 ;example of use:
-(foo)
+(define (pippo)
+  (throw (new pippo-exception%)))
 
-(+ 1 (try ((foo))
-            catch (1)))
+(define (pluto)
+  (throw (new pluto-exception%)))
+
+;(+ 1 (try ((pippo) (pluto))
+;            catch ((pluto-exception% e)) (1)))
+
+(try ((pippo))
+     catch ((pippo-exception% e))
+     ((displayln "pippo caught")
+     (try ((pluto))
+          catch ((pluto-exception% e))
+          ((displayln "pluto caught")))))
