265b0ded84a9134eee7358318deb763a742b36ba
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2013-04-29 22:17:40 +0200

    exception implementation

---
+++ arci-exception.rkt
@@ -0,0 +1,89 @@
+#lang racket
+(require racket/class)
+
+;; ----- Object definitions ----- ;;
+(define exception%
+  (class object%
+    (init name message)   
+    (define current-name name)
+    (define current-message message) 
+    (super-new)
+    
+    (define/public (get-name)
+      current-name)
+    
+    (define/public (get-message)
+      current-message)))
+
+(define negative-number-exception%
+  (class exception%
+    (init message)  
+    (define current-message message)   
+    (super-new [name "NEGATIVE NUMBER EXCEPTION"] [message current-message])))
+
+(define handler%
+  (class object%
+    (init name proc)   
+    (define current-name name)
+    (define current-proc proc) 
+    (super-new)
+    
+    (define/public (get-name)
+      current-name)
+    
+    ;; esegue la procedura dell'handler
+    (define/public (exe-proc exception)
+      (current-proc exception))))
+
+;; ----- Handlers  ----- ;;
+(define *handlers* (list))
+
+(define (push-handler handler)
+  (set! *handlers* (cons handler *handlers*)))
+
+(define (pop-handler)
+  (let ((handler (car *handlers*)))
+    (set! *handlers* (cdr *handlers*))
+    handler))
+
+(define (throw exception)
+  (if (empty? *handlers*)
+      (error (string-append (send exception get-name) ": " (send exception get-message)))
+      ;eseguo l'handler passando l'eccezzione lanciata
+      (send (pop-handler) exe-proc exception)))
+
+(define-syntax try
+  (syntax-rules (catch)
+    ((_ [expr1 ...] catch (exception e) [expr2 ...])
+     (begin
+       (call/cc (lambda (continuation)
+                  ;push oggetto handler
+                  (push-handler 
+                   (new handler%
+                        [name exception]
+                        [proc (lambda (e) 
+                                  (continuation 
+                                   (begin expr2 ...)))]))
+                  (let ([result (begin expr1 ...)])
+                    ;se nessuna throw, rimuovo senza eseguire
+                    (pop-handler)
+                    result)))))))
+
+
+;; ----- Usage ----- ;;
+(define (foo x)
+  (cond
+    [(< x 0) (throw (new negative-number-exception% [message "negative numbers are not allowed"]))]
+    [else (display "here's your x: ") (display x) (newline)]))
+
+(try
+ [(foo 4)] ;non lancerà eccezzione
+ catch (negative-number-exception% nnex)
+ [(display "exception thrown with message: ")
+  (send nnex get-message)])
+
+(try
+ [(foo -4)] ;lancerà eccezzione
+ catch (negative-number-exception% nnex)
+ [(display "exception thrown with message: ")
+  (send nnex get-message)])
