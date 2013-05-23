7d5e24e48beb405c3f71b8c1b7ee99e7ea0860a5
Lorenzo Affetti <affox90@gmail.com>
2013-05-23 13:04:57 +0200

    external catches will catch exception thrown inside of them by other tries

--- obj_ex_readable.rkt
+++ obj_ex_readable.rkt
@@ -30,22 +30,27 @@
 (define (read-h)
   (car *handlers*))
 
+;empties the list
+(define (flush-h)
+  (set! *handlers* '()))
+
 ;throw method
 ;boolean function to understand if we can handle the exception thrown
 (define (can-handle? exception)
-  (if (empty? *handlers*)
-      #f ;no handlers... we can't handle
       (let ((h (read-h))) ;there is at least one handler
-        (if (is-a? exception (send h get-class))
+        (is-a? exception (send h get-class)))
             ;is the catch-exception's class a superclass (or the same class as)
             ;of the thrown-exception's class?
-            #t
-            #f))))
+  )
 
 (define (throw exception)
-  (if (can-handle? exception)
-      ((send (pop-h) get-handler) exception)
-      (error "EXCEPTION:" (send exception get-message))))
+  (if (empty? *handlers*) ;no handlers... we can't handle
+      (error "EXCEPTION:" (send exception get-message))
+      (if (can-handle? exception)
+          ((send (pop-h) get-handler) exception)
+          (begin
+            (pop-h)
+            (throw exception)))))
 
 ;try-catch block (MACRO)
 (define-syntax try
@@ -70,44 +75,60 @@
 
 
 ;some examples of classes;
-; (define exception%
-;   (class object%
-;     (super-new)
-;     (init message)
-;     (field (self-message message))
-;     (define/public (get-message) self-message)))
-; 
-; (define pippo-exception%
-;   (class exception%
-;     (super-new (message "pippo exception"))))
-; 
-; (define pluto-exception%
-;   (class exception%
-;     (super-new (message "pluto exception"))))
+(define exception%
+  (class object%
+    (super-new)
+    (init message)
+    (field (self-message message))
+    (define/public (get-message) self-message)))
 
+(define pippo-exception%
+  (class exception%
+    (super-new (message "pippo exception"))))
+
+(define pluto-exception%
+  (class exception%
+    (super-new (message "pluto exception"))))
+
+(define paperino-exception%
+  (class exception%
+    (super-new (message "paperino exception"))))
 
 ;examples of use:
-; (define (pippo)
-;   (throw (new pippo-exception%)))
-; 
-; (define (pluto)
-;   (throw (new pluto-exception%)))
-; 
-; (+ 1 (try ((pluto) (pippo))
-;             catch ((pluto-exception% e)) (1)))
-; 
-; (try ((pippo))
-;      catch ((pippo-exception% e))
-;      ((displayln "pippo caught")
-;      (try ((pluto))
-;           catch ((pluto-exception% e))
-;           ((displayln "pluto caught")))))
-; 
-; (try ((pippo))
-;        catch ((pippo-exception% e))
-;        ((display "pippo caught: ")
-;         (displayln (send e get-message))
-;         (try ((pluto))
-;              catch ((pluto-exception% e))
-;              ((display "pluto caught: ")
-;               (displayln (send e get-message))))))
+(define (pippo)
+  (throw (new pippo-exception%)))
+
+(define (pluto)
+  (throw (new pluto-exception%)))
+
+(define (paperino)
+  (throw (new paperino-exception%)))
+
+(+ 1 (try ((pluto) (pippo))
+            catch ((pluto-exception% e)) (1)))
+
+(try ((pippo))
+     catch ((pippo-exception% e))
+     ((displayln "pippo caught")
+     (try ((pluto))
+          catch ((pluto-exception% e))
+          ((displayln "pluto caught")))))
+
+(try ((pippo))
+       catch ((pippo-exception% e))
+       ((display "pippo caught: ")
+        (displayln (send e get-message))
+        (try ((pluto))
+             catch ((pluto-exception% e))
+             ((display "pluto caught: ")
+              (displayln (send e get-message))))))
+
+(try (
+     (try (
+           (try ((paperino))
+                catch ((pluto-exception% e))
+                ((displayln "third catch"))))
+          catch ((pluto-exception% e))
+          ((displayln "second catch"))))
+     catch ((paperino-exception% e))
+     ((displayln "first catch")))
